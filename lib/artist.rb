ba257c9f46bb989667eda955964d8dd9ba59da86
Christina Gonzalez <cmgamway@gmail.com>
2019-09-30 07:21:25 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+  attr_accessor :name, :artist, :genre
+
+  ALL = []
+  def initialize(name)
+    @name = name
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
+  end
+end
