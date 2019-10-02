381574c39541a084e1e6002dac3a4c69b30249f9
Christina Gonzalez <cmgamway@gmail.com>
2019-10-02 03:41:13 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,14 +11,15 @@ class Doctor
     @@all_doc
   end
 
-  def new_appointment(doctor, date)
-    Appointment.new(self, doctor, date)
-  end
-
   def appointments
     Appointment.all.select do |appointment|
       appointment.patient
     end
+  end
+
+  def new_appointment(doctor, date)
+    appointment =Appointment.new(date, self, patient)
+    self.appointments.last
   end
 
   def doctors
